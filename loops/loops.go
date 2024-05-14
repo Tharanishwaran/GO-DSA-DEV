7247f8e28f3326f89579543ff0212add2023d1e7
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-05-14 17:31:24 +0530

    loops of go added

---
+++ loops/loops.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main(){
+
+
+ //for loop
+
+//  for i:= 0; i < 2;i++{
+
+//    fmt.Println(i)
+
+//  }
+
+ //while loop
+ i:=0
+
+//  for i > 2{
+
+//     fmt.Println(i)
+
+//     i++
+
+//  }
+
+
+ for { //Forever //If you omit the loop condition it loops forever, so an infinite loop is compactly expressed.
+  
+	fmt.Println(i)
+
+	i++
+
+}
+
+}
+
+
+
+
+
+
+
+
+
