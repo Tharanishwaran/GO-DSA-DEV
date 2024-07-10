62b413fbea6e132c4363720074572c48498f5f39
Tharanishwara <tharanishwaran.ta@gmail.com>
2024-07-10 20:53:27 +0530

    Nested structure added to STruct dir

---
+++ structs/Nested Struct/NS.go
@@ -0,0 +1,33 @@
+// Golang program to illustrate
+// the nested structure
+package main
+
+import "fmt"
+
+// Creating structure
+type Author struct {
+	name string
+	branch string
+	year int
+}
+
+// Creating nested structure
+type HR struct {
+
+	// structure as a field
+	details Author
+}
+
+func main() {
+
+	// Initializing the fields
+	// of the structure
+	result := HR{
+	
+		details: Author{"Sona", "ECE", 2013},
+	}
+
+	// Display the values
+	fmt.Println("\nDetails of Author")
+	fmt.Println(result)
+}
