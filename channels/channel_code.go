d96787f7081173fe8d7458b27161cafdb8c5351c
Tharanishwara <tharanishwaran.ta@gmail.com>
2024-07-17 13:13:56 +0530

    added

---
+++ channels/channel_code.go
@@ -0,0 +1,19 @@
+// Go program to illustrate 
+// how to create a channel 
+package main 
+
+import "fmt"
+
+func main() { 
+
+	// Creating a channel 
+	// Using var keyword 
+	var mychannel chan int
+	fmt.Println("Value of the channel: ", mychannel) 
+	fmt.Printf("Type of the channel: %T ", mychannel) 
+
+	// Creating a channel using make() function 
+	mychannel1 := make(chan int) 
+	fmt.Println("\nValue of the channel1: ", mychannel1) 
+	fmt.Printf("Type of the channel1: %T ", mychannel1) 
+} 
