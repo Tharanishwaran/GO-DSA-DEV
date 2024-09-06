05f4070813c140be8822e83df5aae51cbd2bca2b
Tharanishwaran <tharanishwaran.ta@gmail.com>
2024-09-06 22:52:30 +0530

    binary search add

--- DSA/Algorithms/Binary search/BS.go
+++ DSA/Algorithms/Binary search/BS.go
@@ -2,19 +2,51 @@ package main
 
 import "fmt"
 
-func main(){
+func BinarySearch(arr []int, target int) int {
 
+	low, high := 0, len(arr)-1
 
+	for low <= high {
 
+		mid := low + (high-low)/2
 
+		//check if target is present at mid
+		if arr[mid] == target {
+			return mid
 
- fmt.Println("hello")
+		}
+		//if target is greater, ignore the left half
+		if arr[mid] < target {
 
- for i := 0; i < 5; i++ {
-	
-     fmt.Println("hello" , i)
+			low = mid + 1
 
- }
+		} else {
 
+			//if target is smaller,ignore the right half
+			high = mid - 1
+
+		}
+
+	}
+
+	return -1
+
+}
+
+func main() {
+
+	arr := []int{10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70}
+	target := 25
+
+	result := BinarySearch(arr, target)
+
+	if result != -1 {
+
+		fmt.Println("Element found at index %d\n", result)
+
+	} else {
+
+		fmt.Println("Element not found")
+	}
 
 }
---
+++ DSA/Algorithms/Binary search/go.mod
@@ -0,0 +1,3 @@
+module BSearch.com/binarysearch
+
+go 1.23.0
